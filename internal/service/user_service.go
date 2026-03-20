91bba0570c18b4cff490f1f10ada6b484cd903b1
bot011max <bot011max@gmail.com>
2026-03-20 17:54:21 +0300

    Update user_service.go

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,18 @@
+package service
 
+import (
+    "github.com/bot011max/medical-bot/internal/models"
+    "github.com/bot011max/medical-bot/internal/repository"
+)
+
+type UserService struct {
+    repo *repository.UserRepository
+}
+
+func NewUserService(repo *repository.UserRepository) *UserService {
+    return &UserService{repo: repo}
+}
+
+func (s *UserService) GetUserByID(id string) (*models.User, error) {
+    return s.repo.FindByID(id)
+}
