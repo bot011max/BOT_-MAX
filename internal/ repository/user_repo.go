d02421f6b359579436100046fc05f1a237d94980
bot011max <bot011max@gmail.com>
2026-03-25 12:11:17 +0300

    Update user_repo.go

--- internal/ repository/user_repo.go
+++ internal/ repository/user_repo.go
@@ -1,1 +1,67 @@
+package repository
 
+import (
+    "errors"
+    "github.com/google/uuid"
+    "gorm.io/gorm"
+    "github.com/bot011max/medical-bot/internal/models"
+)
+
+type UserRepository struct {
+    db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+    return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+    return r.db.Create(user).Error
+}
+
+func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
+    var user models.User
+    err := r.db.Where("email = ?", email).First(&user).Error
+    if errors.Is(err, gorm.ErrRecordNotFound) {
+        return nil, nil
+    }
+    return &user, err
+}
+
+func (r *UserRepository) FindByID(id uuid.UUID) (*models.User, error) {
+    var user models.User
+    err := r.db.First(&user, "id = ?", id).Error
+    if errors.Is(err, gorm.ErrRecordNotFound) {
+        return nil, nil
+    }
+    return &user, err
+}
+
+func (r *UserRepository) FindByTelegramID(telegramID int64) (*models.User, error) {
+    var user models.User
+    err := r.db.Where("telegram_id = ?", telegramID).First(&user).Error
+    if errors.Is(err, gorm.ErrRecordNotFound) {
+        return nil, nil
+    }
+    return &user, err
+}
+
+func (r *UserRepository) Update(user *models.User) error {
+    return r.db.Save(user).Error
+}
+
+func (r *UserRepository) UpdateTelegramID(userID uuid.UUID, telegramID int64) error {
+    return r.db.Model(&models.User{}).
+        Where("id = ?", userID).
+        Update("telegram_id", telegramID).Error
+}
+
+func (r *UserRepository) Delete(id uuid.UUID) error {
+    return r.db.Delete(&models.User{}, "id = ?", id).Error
+}
+
+func (r *UserRepository) Count() (int64, error) {
+    var count int64
+    err := r.db.Model(&models.User{}).Count(&count).Error
+    return count, err
+}
